a843c07a74b5f72217e809e1ae38d12245d70cca
Yellow May <kwerep09@yahoo.com>
2021-05-30 18:38:56 +0100

    - Filtered extracted Redux todosData - Depending on the todo's state currentState - return type TodoState for each switch cases of 'all', 'active' and 'completed' - Mapped through the return data to output the Todo component - Destructure the TodoType data of each Todo to be used as props in Todo component

--- src/components/ViewTodos.tsx
+++ src/components/ViewTodos.tsx
@@ -1,12 +1,30 @@
 import React from "react";
+import { useAppSelector } from "../redux/hooks";
+import { currentState, todosData } from "../redux/slices/todoSlice";
 import Todo from "./Todo";
 
 const ViewTodos = () => {
+	const todos = useAppSelector(todosData);
+	const appState = useAppSelector(currentState);
+
+	const filtered = () => {
+		switch (appState) {
+			case "active":
+				return todos.filter(todo => (todo.pending = false));
+			case "all":
+				return todos;
+			case "completed":
+				return todos.filter(todo => (todo.pending = true));
+			default:
+				return todos;
+		}
+	};
+
 	return (
 		<div className='view-todos'>
-			<Todo />
-			<Todo />
-			<Todo />
+			{filtered().map(todo => (
+				<Todo key={todo.id} {...todo} />
+			))}
 		</div>
 	);
 };
