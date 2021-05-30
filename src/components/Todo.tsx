c54d47becb8693016825f82d109154a89555aa6d
Yellow May <kwerep09@yahoo.com>
2021-05-30 20:34:14 +0100

    - cross icon imported to component

--- src/components/Todo.tsx
+++ src/components/Todo.tsx
@@ -1,14 +1,18 @@
 import React from "react";
+import { TodoType } from "../redux/slices/todoSlice";
+import closeIcon from "../assets/icons/icon-cross.svg";
 
-const Todo = () => {
+const Todo = (props: TodoType) => {
 	return (
 		<div className='todo-item'>
 			<div className='check'>
 				<input type='checkbox' name='markTodo' />
 				<span></span>
 			</div>
-			<h3 className='title'>Title</h3>
-			<button className='btn btn-del' title='delete Todo'></button>
+			<h3 className='title'>{props.title}</h3>
+			<button className='btn btn-del' title='delete Todo'>
+				<img src={closeIcon} alt='close' />
+			</button>
 		</div>
 	);
 };
