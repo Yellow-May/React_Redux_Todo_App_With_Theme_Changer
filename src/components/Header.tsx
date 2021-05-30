6be0a3c025e2045f4583cc56d84c87fb8ea56879
Yellow May <kwerep09@yahoo.com>
2021-05-30 18:00:12 +0100

    - Elements structure added - Redux theme state pulled using useAppSelector - Redux dispatcher also pulled using useAppDispatcher - theme state used to toggle the button icon - dispatch used to call the toggleTheme function to update the Redux theme state

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,21 @@
 import React from "react";
+import { useAppSelector, useAppDispatch } from "../redux/hooks";
+import { currentTheme, toggleTheme } from "../redux/slices/themeSlice";
+import moonIcon from "../assets/icons/icon-moon.svg";
+import sunIcon from "../assets/icons/icon-sun.svg";
 
 const Header = () => {
-	return <div></div>;
+	const theme = useAppSelector(currentTheme);
+	const dispatch = useAppDispatch();
+
+	return (
+		<header>
+			<h1>Todo App</h1>
+			<button title='switch theme' onClick={() => dispatch(toggleTheme())}>
+				<img src={theme === "dark" ? sunIcon : moonIcon} alt='moon' />
+			</button>
+		</header>
+	);
 };
 
 export default Header;
