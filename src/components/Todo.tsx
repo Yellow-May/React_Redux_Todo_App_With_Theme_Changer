3218cbec5d26ccffa72786aa713d1281dd863e03
Yellow May <kwerep09@yahoo.com>
2021-05-30 17:00:45 +0100

    - Individual resuseable component

---
+++ src/components/Todo.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Todo = () => {
+	return (
+		<div>
+			<div className='check'>
+				<input type='checkbox' name='markTodo' />
+				<span></span>
+			</div>
+		</div>
+	);
+};
+
+export default Todo;
