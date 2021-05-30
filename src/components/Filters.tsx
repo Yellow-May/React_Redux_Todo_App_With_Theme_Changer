8aca80271669cb097f1c95f060408d7f8c98a5f2
Yellow May <kwerep09@yahoo.com>
2021-05-30 20:23:28 +0100

    - Pulled currentState data from todo redux state - createed function to handleChange of states through dispatch - created function to display current number of items in each state

--- src/components/Filters.tsx
+++ src/components/Filters.tsx
@@ -1,19 +1,42 @@
 import React from "react";
+import { useAppDispatch, useAppSelector } from "../redux/hooks";
+import { changeState, currentState, todosData } from "../redux/slices/todoSlice";
+
+const filterBtns = ["all", "active", "completed"];
 
 const Filters = () => {
+	const todos = useAppSelector(todosData);
+	const appState = useAppSelector(currentState);
+	const dispatch = useAppDispatch();
+
+	const itemsLeft = () => {
+		if (appState === "active") return todos.filter(todo => todo.pending === true).length;
+		if (appState === "completed") return todos.filter(todo => todo.pending === false).length;
+		return todos.length;
+	};
+
+	const handleStateChange = (e: React.MouseEvent<HTMLButtonElement>) => {
+		const name = e.currentTarget.name;
+
+		name === "all" && dispatch(changeState("all"));
+		name === "active" && dispatch(changeState("active"));
+		name === "completed" && dispatch(changeState("completed"));
+	};
+
 	return (
 		<div className='filters-section'>
-			<small>{0} items left</small>
+			<small>{itemsLeft()} items left</small>
 			<div className='filters'>
-				<button className='btn btn-filter' title='view all'>
-					all
-				</button>
-				<button className='btn btn-filter' title='view active'>
-					active
-				</button>
-				<button className='btn btn-filter' title='view completed'>
-					completed
-				</button>
+				{filterBtns.map(btn => (
+					<button
+						className={`btn btn-filter ${btn === appState ? "on" : ""}`}
+						key={btn}
+						name={btn}
+						title={`view ${btn}`}
+						onClick={handleStateChange}>
+						{btn}
+					</button>
+				))}
 			</div>
 			<button className='btn btn-filter' title='clear completed'>
 				clear completed
