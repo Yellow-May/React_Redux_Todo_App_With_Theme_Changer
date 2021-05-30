6cd886a996a90144d6d9a42ae6428e1ebd1a99d5
Yellow May <kwerep09@yahoo.com>
2021-05-30 17:57:56 +0100

    - Redux store theme state pullled - theme state used to toggle App className

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,23 @@
 import React from "react";
+import { useAppSelector } from "./redux/hooks";
 import AddTodo from "./components/AddTodo";
 import Filters from "./components/Filters";
 import Header from "./components/Header";
-import ViewTodo from "./components/ViewTodo";
+import ViewTodos from "./components/ViewTodos";
+import { currentTheme } from "./redux/slices/themeSlice";
 
 function App() {
+	const theme = useAppSelector(currentTheme);
+	const classes = theme === "dark" ? "App dark" : "App light";
+
 	return (
-		<div className='App'>
+		<div className={classes}>
 			<Header />
 			<AddTodo />
-			<ViewTodo />
-			<Filters />
+			<main>
+				<ViewTodos />
+				<Filters />
+			</main>
 		</div>
 	);
 }
