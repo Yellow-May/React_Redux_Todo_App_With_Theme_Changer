3372452e00430684f4e02e733d65e95f65674ce4
Yellow May <kwerep09@yahoo.com>
2021-05-30 18:16:50 +0100

    - Elements structure added - Redux dispatch to add new Todo added - Implemented uuid for todo id

--- src/components/AddTodo.tsx
+++ src/components/AddTodo.tsx
@@ -1,7 +1,43 @@
 import React from "react";
+import { useAppDispatch } from "../redux/hooks";
+import { addTodo } from "../redux/slices/todoSlice";
+import { v4 as uuidv4 } from "uuid";
 
 const AddTodo = () => {
-	return <div></div>;
+	const [title, setTitle] = React.useState<string>("");
+
+	const dispatch = useAppDispatch();
+
+	return (
+		<form
+			onSubmit={(e: React.FormEvent<HTMLFormElement>) => {
+				e.preventDefault();
+				dispatch(
+					addTodo({
+						id: uuidv4(),
+						title,
+						pending: false,
+					})
+				);
+				setTitle("");
+			}}>
+			<div className='check'>
+				<input type='checkbox' name='markTodo' />
+				<span></span>
+			</div>
+			<input
+				className='title'
+				type='text'
+				name='newTodo'
+				value={title}
+				onChange={(e: React.ChangeEvent<HTMLInputElement>) => setTitle(e.target.value)}
+				autoComplete='off'
+			/>
+			<button className='btn btn-form' type='submit' title='add new todo'>
+				add
+			</button>
+		</form>
+	);
 };
 
 export default AddTodo;
--- src/components/Todo.tsx
+++ src/components/Todo.tsx
@@ -2,11 +2,13 @@ import React from "react";
 
 const Todo = () => {
 	return (
-		<div>
+		<div className='todo-item'>
 			<div className='check'>
 				<input type='checkbox' name='markTodo' />
 				<span></span>
 			</div>
+			<h3 className='title'>Title</h3>
+			<button className='btn btn-del' title='delete Todo'></button>
 		</div>
 	);
 };
