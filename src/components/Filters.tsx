5d2a15beeebf92b925034cc3370ae88286c8df01
Yellow May <kwerep09@yahoo.com>
2021-05-30 18:04:43 +0100

    - Elements structure added

--- src/components/Filters.tsx
+++ src/components/Filters.tsx
@@ -1,7 +1,25 @@
 import React from "react";
 
 const Filters = () => {
-	return <div></div>;
+	return (
+		<div className='filters-section'>
+			<small>{0} items left</small>
+			<div className='filters'>
+				<button className='btn btn-filter' title='view all'>
+					all
+				</button>
+				<button className='btn btn-filter' title='view active'>
+					active
+				</button>
+				<button className='btn btn-filter' title='view completed'>
+					completed
+				</button>
+			</div>
+			<button className='btn btn-filter' title='clear completed'>
+				clear completed
+			</button>
+		</div>
+	);
 };
 
 export default Filters;
