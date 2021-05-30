616ef6c2cf0c17f8f4556b75662898abd7cf177d
Yellow May <kwerep09@yahoo.com>
2021-05-30 20:42:17 +0100

    - Delete functionality added through dispatch delTodo - Check complete added through dispatch markTodo

--- src/components/Todo.tsx
+++ src/components/Todo.tsx
@@ -1,16 +1,24 @@
 import React from "react";
-import { TodoType } from "../redux/slices/todoSlice";
+import { useAppDispatch } from "../redux/hooks";
+import { delTodo, markTodo, TodoType } from "../redux/slices/todoSlice";
 import closeIcon from "../assets/icons/icon-cross.svg";
 
 const Todo = (props: TodoType) => {
+	const dispatch = useAppDispatch();
+
 	return (
 		<div className='todo-item'>
 			<div className='check'>
-				<input type='checkbox' name='markTodo' />
+				<input
+					type='checkbox'
+					name='markTodo'
+					checked={!props.pending}
+					onChange={() => dispatch(markTodo(props.id))}
+				/>
 				<span></span>
 			</div>
 			<h3 className='title'>{props.title}</h3>
-			<button className='btn btn-del' title='delete Todo'>
+			<button className='btn btn-del' title='delete Todo' onClick={() => dispatch(delTodo(props.id))}>
 				<img src={closeIcon} alt='close' />
 			</button>
 		</div>
