231202d84635e4aad89489df5484e82569677479
Yellow May <kwerep09@yahoo.com>
2021-05-31 09:38:24 +0100

    - Css Reset

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,7 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import { store } from "./redux/store";
 import { Provider } from "react-redux";
+import "./styles/index.css";
 
 ReactDOM.render(
 	<React.StrictMode>
